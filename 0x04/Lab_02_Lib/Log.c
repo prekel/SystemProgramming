ee874f4d6f0eda62a86dea12a68c462d74ac435e
Vladislav Prekel <misterptits@yandex.ru>
2019-10-04 23:03:29 +0700

    Log macro 95%

--- 0x04/Lab_02_Lib/Log.c
+++ 0x04/Lab_02_Lib/Log.c
@@ -5,6 +5,12 @@
 #include "Log.h"
 #include "Macro.h"
 
+#define LOG_OUTPUT_STREAM stdout
+
+static Table* g_pLoggingTable;
+static pthread_mutex_t g_pLogMutex;
+static bool g_IsLoggerInitialized = false;
+
 char ForkToChar(Fork* fork)
 {
     if (fork->IsInUse)
@@ -12,7 +18,7 @@ char ForkToChar(Fork* fork)
     else
         return '.';
 }
-char PhToChar(Philosopher* fork)
+char PhilosopherToChar(Philosopher* fork)
 {
     if (fork->IsEating)
         return '=';
@@ -28,25 +34,12 @@ char* TableInfo(Table* pTable)
     FAILURE_IF_NULLPTR(result);
     for (int i = 0; i < pTable->PhilosophersCount; i++)
     {
-        result[i * 2] = PhToChar(pTable->ppPhilosophers[i]);
+        result[i * 2] = PhilosopherToChar(pTable->ppPhilosophers[i]);
         result[i * 2 + 1] = ForkToChar(pTable->ppForks[i]);
     }
     result[pTable->PhilosophersCount * 2] = '\0';
     return result;
 }
-
-void LogTableInfo(Table* pTable)
-{
-    char* tableInfo = TableInfo(pTable);
-    fprintf(stdout, "[%s]", tableInfo);
-    free(tableInfo);
-}
-
-#define LOG_OUTPUT_STREAM stdout
-
-static Table* g_pLoggingTable;
-static pthread_mutex_t g_pLogMutex;
-static bool g_IsLoggerInitialized = false;
 
 void InitLogger(Table* pTable)
 {
@@ -56,53 +49,18 @@ void InitLogger(Table* pTable)
 	g_IsLoggerInitialized = true;
 }
 
-void LogPrefix(char* fileName)
+void LogPrefix()
 {
     char* info = TableInfo(g_pLoggingTable);
 #ifdef __MINGW32__
-    printf("[%s][tid: 0x%08llx][%24s] ", info, pthread_self(), fileName);
-#else
-    printf("[%s][tid: 0x%08lx][%24s] ", info, pthread_self(), fileName);
-#endif
-    free(info);
-}
-
-void Log(char* fileName, char* format, ...)
-{
-	if (!g_IsLoggerInitialized)
-	{
-		return;
-	}
-	pthread_mutex_lock(&g_pLogMutex);
-
-	LogPrefix(fileName);
-
-	//char* info = TableInfo(g_pLoggingTable);
-	//fprintf(LOG_OUTPUT_STREAM, "[%s][tid: 0x%08lx][%24s] ", info, pthread_self(), fileName);
-	//free(info);
-
-    va_list argptr;
-    va_start(argptr, format);
-	vfprintf(LOG_OUTPUT_STREAM, format, argptr);
-	fprintf(LOG_OUTPUT_STREAM, "\n");
-    va_end(argptr);
-
-    pthread_mutex_unlock(&g_pLogMutex);
-}
-
-
-void LogPrefix1()
-{
-    char* info = TableInfo(g_pLoggingTable);
-#ifdef __MINGW32__
-    printf("[%s][tid: 0x%08llx][%24s] ", info, pthread_self(), fileName);
+    printf("[%s][tid: 0x%08llx]", info, pthread_self());
 #else
     printf("[%s][tid: 0x%08lx]", info, pthread_self());
 #endif
     free(info);
 }
 
-void Log2(char* format, ...)
+void Log(char* format, ...)
 {
     if (!g_IsLoggerInitialized)
     {
@@ -110,13 +68,13 @@ void Log2(char* format, ...)
     }
     pthread_mutex_lock(&g_pLogMutex);
 
-    LogPrefix1();
+    LogPrefix();
 
-    va_list argptr;
-    va_start(argptr, format);
-    vfprintf(LOG_OUTPUT_STREAM, format, argptr);
+    va_list argPtr;
+    va_start(argPtr, format);
+    vfprintf(LOG_OUTPUT_STREAM, format, argPtr);
     fprintf(LOG_OUTPUT_STREAM, "\n");
-    va_end(argptr);
+    va_end(argPtr);
 
     pthread_mutex_unlock(&g_pLogMutex);
 }
--- 0x04/Lab_02_Lib/Log.h
+++ 0x04/Lab_02_Lib/Log.h
@@ -10,32 +10,18 @@
 
 #define _FILE strrchr(__FILE__, '/') ? strrchr(__FILE__, '/') + 1 : __FILE__
 
-#define PPCAT_NX(A, B) A ## B
-#define PPCAT(A, B) PPCAT_NX(A, B)
 #define STRINGIZE_NX(A) #A
 #define STRINGIZE(A) STRINGIZE_NX(A)
 
 #define LOG_FILE_ALIGN 30
 #define LOG_FMT             "[%*s:%d] "
-#define LOG_ARGS     (LOG_FILE_ALIGN - sizeof(STRINGIZE(__LINE__))),_FILE, __LINE__
+#define LOG_ARGS     (LOG_FILE_ALIGN - sizeof(STRINGIZE(__LINE__))), _FILE, __LINE__
 
-#define LOG_HELPER(format, ...)      Log2(LOG_FMT format, __VA_ARGS__)
+#define LOG_HELPER(format, ...)      Log(LOG_FMT format, __VA_ARGS__)
 #define LOG(message, args...)     LOG_HELPER(message, LOG_ARGS, ## args)
-//
-//char ForkToChar(Fork* fork);
-//
-//char PhToChar(Philosopher* fork);
-//
-//char* TableInfo(Table* pTable);
-//
-//void LogTableInfo(Table* pTable);
-//
+
 void InitLogger(Table* pTable);
-//
-//void LogPrefix(char* fileName);
 
-void Log(char* fileName, char* format, ...);
-
-void Log2(char* format, ...);
+void Log(char* format, ...);
 
 #endif // LOG_H
