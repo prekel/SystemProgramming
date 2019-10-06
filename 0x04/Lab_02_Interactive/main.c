b7d5e595868ac9b590b8540d97f8df33fb410631
Vladislav Prekel <misterptits@yandex.ru>
2019-10-06 13:17:44 +0700

    123

--- 0x04/Lab_02_Interactive/main.c
+++ 0x04/Lab_02_Interactive/main.c
@@ -17,13 +17,13 @@
 const int SCREEN_WIDTH = 512;
 const int SCREEN_HEIGHT = 512;
 
-const int MAX_INT_LENGTH = 10;
+const int MAX_INT_LENGTH = 20;
 
 const int SEC_IN_DAY = 60 * 60 * 24;
 
 bool TimeChecker(int time)
 {
-    return 0 <= time && time <= SEC_IN_DAY;
+    return 0 <= time;// && time <= SEC_IN_DAY;
 }
 
 bool PhilosophersCountChecker(int philosophersCount)
@@ -43,19 +43,22 @@ int main(int argc, char** args)
     int philosophersCount = CycleInputInt("Введите кол-во философов: ", MAX_INT_LENGTH, PhilosophersCountChecker);
     printf("\n");
 
+    printf("Время вводится в миллисекундах\n");
+    printf("Разность между верхней границей и нижней должна быть не меньше 0 и не больше %d\n\n", RAND_MAX);
+
     int minDurationEat;
     int maxDurationEat;
     do
     {
-        printf("Для того, чтобы время приёма пищы было бесконечным, введите 0 минимальное и максимальное\n");
-        printf("Для того, чтобы было постоянным, введите одинаковое максимальное и минимальное\n");
+        printf("Для того, чтобы время приёма пищы было бесконечным, введите 0 и 0\n");
+        printf("Для того, чтобы было постоянным, введите одинаковые числа\n");
         minDurationEat = CycleInputInt(
                 "Введите минимальное время приёма пищи (например 1000): ",
                 MAX_INT_LENGTH, TimeChecker);
         maxDurationEat = CycleInputInt(
-                "Введите максимальное время приёма пищи (например 9000): ",
+                "Введите максимальное время приёма пищи (например 5000): ",
                 MAX_INT_LENGTH, TimeChecker);
-    } while (maxDurationEat < minDurationEat);
+    } while (maxDurationEat < minDurationEat || maxDurationEat - minDurationEat > RAND_MAX);
     bool isInfinityDuration = minDurationEat == 0 && maxDurationEat == 0;
     printf("\n");
 
@@ -64,22 +67,28 @@ int main(int argc, char** args)
     do
     {
         printf("Для того, философы не появлялись автоматически, введите 0 и 0\n");
-        printf("Для того, чтобы было постоянным, введите одинаковое максимальное и минимальное\n");
+        printf("Для того, чтобы было постоянным, введите одинаковые числа\n");
         minSendIntervalDuration = CycleInputInt(
-                "Введите минимальное время между появлениями (например 1000): ",
+                "Введите минимальное время между появлениями (например 500): ",
                 MAX_INT_LENGTH, TimeChecker);
         maxSendIntervalDuration = CycleInputInt(
-                "Введите максимальное время между появлениями (например 3000): ",
+                "Введите максимальное время между появлениями (например 1500): ",
                 MAX_INT_LENGTH, TimeChecker);
-    } while (maxSendIntervalDuration < minSendIntervalDuration);
+    } while (maxSendIntervalDuration < minSendIntervalDuration || maxSendIntervalDuration - minSendIntervalDuration > RAND_MAX);
     bool isAutoSpawnDisabled = minSendIntervalDuration == 0 && maxSendIntervalDuration == 0;
+    printf("\n");
+
+    printf("Управление: [1-9]      - отправить философа есть\n");
+    printf("            Alt+[1-9]  - прекратить приём пищи или ожидание\n");
+    printf("            Ctrl+[1-9] - переключение метки бесконечного приёма пищи\n");
+    printf("            Esc        - выход из программы с ожиданием завершения всех потоков\n");
     printf("\n");
 
     Table* pTable = CreateTable(philosophersCount, minDurationEat, maxDurationEat,
                                 isInfinityDuration);
 
-    InitLogger(pTable, stdout, true, fopen("5.txt", "w+"), true);
-    //InitLogger(pTable, stdout, true, NULL, false);
+    //InitLogger(pTable, stdout, false, fopen("5.txt", "w+"), false);
+    InitLogger(pTable, stdout, true, NULL, false);
     LOG("Введены данные, создание объектов, запуск потоков");
 
     MainWindow* pMainWindow = CreateMainWindow(
