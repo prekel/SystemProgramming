cb114f7ad1d8c8be0a1957b1a55e7730a6abdbdf
Vladislav Prekel <misterptits@yandex.ru>
2019-10-15 23:22:41 +0700

    need to fix logger

--- 0x04/Lab_02/Lab_02_Android/app/jni/src/YourSourceHere.c
+++ 0x04/Lab_02/Lab_02_Android/app/jni/src/YourSourceHere.c
@@ -1,15 +1,34 @@
-#include <SDL.h>
-
 #include <stdlib.h>
 #include <stdbool.h>
+
+#include <SDL.h>
+
+#include <android/log.h>
 
 #include "Table.h"
 #include "Logger.h"
 #include "MainWindow.h"
 
-int SDL_main(int argc, char** argv) {
+int LogFunction1(char* format, ...)
+{
+    va_list argPtr;
+    va_start(argPtr, format);
+    int ret = __android_log_vprint(ANDROID_LOG_INFO, "Lab_02_Android", format, argPtr);
+    va_end(argPtr);
+    return ret;
+}
 
+int LogFunction2(char* format, va_list argPtr)
+{
+    return __android_log_vprint(ANDROID_LOG_INFO, "Lab_02_Android", format, argPtr);
+}
+
+int SDL_main(int argc, char** argv) {
     Table* pTable = CreateTable(9, 1000, 5000, false);
+
+    InitLogger(pTable, stdout, true, NULL, false);
+    g_pFunction1 = LogFunction1;
+    g_pFunction2 = LogFunction2;
 
     MainWindow* pMainWindow = CreateMainWindow(1080, 2280, pTable, 500, 1500, false, false, true);
     pMainWindow->IsRendererAsync = false;
--- 0x04/Lab_02/Lab_02_Lib/Logger.c
+++ 0x04/Lab_02/Lab_02_Lib/Logger.c
@@ -16,15 +16,21 @@ static bool g_IsLoggerInitialized = false;
 /// Основной поток для вывода
 static FILE* g_pMainOutputStream;
 /// Требуется ли выводить информацию о столе в основной поток
-static bool g_IsMainTableInfoEnabled;
+static bool g_IsMainOutputStreamTableInfoEnabled;
 /// Дополнителый поток для вывода
 static FILE* g_pSecondaryOutputStream;
 /// Требуется ли выводить информацию о столе в дополнительный поток
-static bool g_IsSecondaryTableInfoEnabled;
+static bool g_IsSecondaryOutputStreamTableInfoEnabled;
 /// Требуется ли генерировать информацию о столе
 static bool g_IsTableInfoRequired;
 /// Дополнительный мьютекс (не главный) для логгирования
 static pthread_mutex_t g_pLoggerMutex;
+
+static bool(* g_pMainOutputFunction)(char*, va_list);
+static bool g_IsMainOutputFunctionTableInfoEnabled;
+static bool(* g_pSecondaryOutputFunction)(char*, va_list);
+static bool g_IsSecondaryOutputFunctionTableInfoEnabled;
+
 
 /// Преобразует вилку в символ.
 ///
@@ -79,10 +85,10 @@ void InitLogger(Table* pTable, FILE* pMainOutputStream,
     g_pLoggingTable = pTable;
 
     g_pMainOutputStream = pMainOutputStream;
-    g_IsMainTableInfoEnabled = isMainTableInfoEnabled;
+    g_IsMainOutputStreamTableInfoEnabled = isMainTableInfoEnabled;
 
     g_pSecondaryOutputStream = pSecondaryOutputStream;
-    g_IsSecondaryTableInfoEnabled = isSecondaryTableInfoEnabled;
+    g_IsSecondaryOutputStreamTableInfoEnabled = isSecondaryTableInfoEnabled;
 
     g_IsTableInfoRequired =
             isMainTableInfoEnabled || isSecondaryTableInfoEnabled;
@@ -119,14 +125,22 @@ void Log(char* format, ...)
     result[tableInfoLength - 1] = '\0';
 
     char empty[] = "";
-    char* res1 = g_IsMainTableInfoEnabled ? result : empty;
-    char* res2 = g_IsSecondaryTableInfoEnabled ? result : empty;
+    char* res1 = g_IsMainOutputStreamTableInfoEnabled ? result : empty;
+    char* res2 = g_IsSecondaryOutputStreamTableInfoEnabled ? result : empty;
 
     pthread_mutex_lock(&g_pLoggerMutex);
 
 #ifdef __MINGW32__
-    if (g_pMainOutputStream) fprintf(g_pMainOutputStream, "[%s][tid: 0x%08llx]", res1, pthread_self());
-    if (g_pSecondaryOutputStream) fprintf(g_pSecondaryOutputStream, "[%s][tid: 0x%08llx]", res2, pthread_self());
+    if (g_pMainOutputStream)
+    {
+        fprintf(g_pMainOutputStream, "[%s][tid: 0x%08llx]", res1,
+                pthread_self());
+    }
+    if (g_pSecondaryOutputStream)
+    {
+        fprintf(g_pSecondaryOutputStream, "[%s][tid: 0x%08llx]", res2,
+                pthread_self());
+    }
 #else
     if (g_pMainOutputStream)
     {
@@ -137,6 +151,11 @@ void Log(char* format, ...)
     {
         fprintf(g_pSecondaryOutputStream, "[%s][tid: 0x%08lx]", res2,
                 pthread_self());
+    }
+    if (g_pFunction1)
+    {
+        g_pFunction1("[%s][tid: 0x%08lx]", res1,
+                     pthread_self());
     }
 #endif
 
@@ -150,6 +169,15 @@ void Log(char* format, ...)
     if (g_pSecondaryOutputStream)
     {
         vfprintf(g_pSecondaryOutputStream, format, argPtr);
+    }
+    if (g_pSecondaryOutputStream) fprintf(g_pSecondaryOutputStream, "\n");
+    va_end(argPtr);
+
+
+    va_start(argPtr, format);
+    if (g_pFunction2)
+    {
+        g_pFunction2(format, argPtr);
     }
     if (g_pSecondaryOutputStream) fprintf(g_pSecondaryOutputStream, "\n");
     va_end(argPtr);
