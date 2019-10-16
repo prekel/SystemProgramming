51a97f237c59eeadfe5e40a0956d26efa73b2ca1
Vladislav Prekel <misterptits@yandex.ru>
2019-10-16 12:51:29 +0700

    removed smth

--- 0x04/Lab_02/Lab_02_Android/app/jni/src/MainAndroid.c
+++ 0x04/Lab_02/Lab_02_Android/app/jni/src/MainAndroid.c
@@ -17,7 +17,7 @@ int AndroidLog(char* format)
 
 int SDL_main(int argc, char** argv)
 {
-    Table* pTable = CreateTable(5, 1000, 5000, false);
+    Table* pTable = CreateTable(6, 1000, 5000, false);
     //Table* pTable = CreateTable(6000, 1000, 10000, false);
 
     //InitLogger(pTable, false, NULL, NULL, AndroidLog, NULL);
