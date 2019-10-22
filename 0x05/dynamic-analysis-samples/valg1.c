217d237ffe20e1acdf8ceef099075e26c0ec489d
Vladislav Prekel <misterptits@yandex.ru>
2019-10-22 15:17:52 +0700

    untar examples

---
+++ 0x05/dynamic-analysis-samples/valg1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+  int* p;
+  int c = *p;
+  if (0 == c)
+  {
+    printf("Kuku!!!\n");
+  }
+  return 0;
+}
---
+++ 0x05/static-analysis-samples/sample1.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main()
+{
+  FILE* fp = fopen("kuku", "r");
+
+  return 0;
+}
