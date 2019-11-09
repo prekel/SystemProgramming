a83e5ccadd9d12ba4e1ae75041152fb6077eaf16
Vladislav Prekel <misterptits@yandex.ru>
2019-11-09 14:56:03 +0700

    lib cl fix 3

--- 0x08/Lab_04/Lab_04_Lib/Makefile-windows.mk
+++ 0x08/Lab_04/Lab_04_Lib/Makefile-windows.mk
@@ -1,23 +1,24 @@
-CC = cl
-AR = lib
+CC = cl.exe
+LINK = link.exe
+AR = lib.exe
 
 CFLAGS= /Od /Wall
 LDFLAGS=
 
 SOURCES = Archipelago.c RecordFile.c Meta.c ParseInt.c IOWrapper.c
-OBJECTS = $(SOURCES:.c=.obj)
+OBJECTS = $(SOURCES:.c=.o)
 
 OUTFILE = Lab_04_Lib.lib
 
 all: $(SOURCES) $(OUTFILE)
 
 $(OUTFILE): $(OBJECTS)
-	$(AR) /OUT $(OUTFILE) $(OBJECTS)
+	$(AR) /OUT: $(OUTFILE) $(OBJECTS)
 
-.c.obj:
-	$(CC) /c $(CFLAGS) $< $@
+.c.o:
+	$(CC) /c $(CFLAGS) $< /Fo$@
 
 .PHONY: clean
 clean:
 	del $(OUTFILE)
-	del *.obj
+	del *.o
