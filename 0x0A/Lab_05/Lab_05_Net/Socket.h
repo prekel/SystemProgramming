fa2271182c75657cc92dc2b668e09f04479b3f95
Vladislav Prekel <misterptits@yandex.ru>
2019-11-16 23:05:19 +0700

    no shutdown constants

--- 0x0A/Lab_05/Lab_05_Net/Socket.h
+++ 0x0A/Lab_05/Lab_05_Net/Socket.h
@@ -2,25 +2,19 @@
 #define SOCKET_H
 
 #ifdef _MSC_VER
-
 #include <WinSock2.h>
-
 #elif _WIN32
-
 #include <winsock2.h>
 #include <sys/types.h>
 #include <fcntl.h>
 #include <unistd.h>
-
 #else
-
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <fcntl.h>
 #include <sys/types.h>
 #include <unistd.h>
 #include <arpa/inet.h>
-
 #endif
 
 
@@ -28,22 +22,11 @@
 #define NO_ERROR 0
 #endif
 
-#ifdef SD_RECEIVE
-#define SHUT_RD SD_RECEIVE
-#endif
-#ifdef SD_SEND
-#define SHUT_WR SD_SEND
-#endif
-#ifdef SD_BOTH
-#define SHUT_RDWR SD_BOTH
-#endif
-
 #ifdef _WIN32
 typedef SOCKET SocketHandle;
 #else
 typedef int SocketHandle;
 #endif
-
 
 #ifndef _WIN32
 #define closesocket(sock) close(sock)
