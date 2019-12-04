cd150f1558293c90c5bcdb62fb0ceeb0d01a7c9c
Vladislav Prekel <misterptits@yandex.ru>
2019-12-04 20:20:34 +0700

    0x0f-0x11

---
+++ 0x0F/MIPS-32/example_5_6.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int function(int a);
+
+int main()
+{
+  int x=5;
+  int y;
+  y = function(x);
+  printf("y=%i\n", y);
+  return 0;
+}
+
+int function(int a)
+{
+  return 3*a+5;
+}
