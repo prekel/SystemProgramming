f616c5aee366ba93d385e36e22da8cd240c8482d
Vladislav Prekel <misterptits@yandex.ru>
2020-10-18 21:20:55 +0700

    Emscripten started

---
+++ 0x04/Lab_02/Lab_02_Wasm/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+//#include <emscripten.h>
+//#include <SDL2/SDL.h>
+
+int main(int argc, char** args)
+{
+    printf("Обозначения: Большой квадрат - философ:\n");
+
+    return 0;
+}
